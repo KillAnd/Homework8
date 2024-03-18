9fa60d0760701821f2b20a9cb27fe86774e575be
kila1 <kila14081996@gmail.com>
2024-03-18 20:54:34 +0300

    Домашняя работа №8. Задание №2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         //task 1
+        System.out.println("Task 1");
         int [] one = new int[3];
         one[0] = 1;
         one[1] = 2;
@@ -12,6 +13,33 @@ public class Main {
 
         int [] threeTime = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12};
         System.out.println(threeTime[4]);
+
+        //task 2
+        System.out.println("Task 2");
+        for(int i=0; i < one.length; i++) {
+            if (i == one.length - 1) {
+                System.out.println(one[i]);
+                break;
+            }
+            System.out.print(one[i] + ", ");
+        }
+
+        for (int i=0; i < two.length; i++) {
+            if (i == two.length - 1) {
+                System.out.println(two[i]);
+                break;
+            }
+            System.out.print(two[i] + ", ");
+        }
+
+        for (int i = 0; i < threeTime.length; i++) {
+            if (i == threeTime.length - 1) {
+                System.out.println(threeTime[i]);
+                break;
+            }
+            System.out.print(threeTime[i] + ", ");
+        }
+
 
     }
 }
