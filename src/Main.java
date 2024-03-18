3d7c73b2067429d5265a1f2f8399aff8be0283b2
kila1 <kila14081996@gmail.com>
2024-03-18 20:29:25 +0300

    Домашняя работа №8. Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello work #8!");
+        //task 1
+        int [] one = new int[3];
+        one[0] = 1;
+        one[1] = 2;
+        one[2] = 3;
+        System.out.println(one[0]);
+
+        double [] two = {1.57, 7.654, 9.986};
+        System.out.println(two[0]);
+
+        int [] threeTime = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12};
+        System.out.println(threeTime[4]);
+
     }
 }
